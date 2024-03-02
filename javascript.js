831bf4e892ae00e4f3f2d61c9ab30747fd22acaf
JPeri05 <joaopauloaranha500@gmail.com>
2024-03-02 11:31:21 -0300

    update

--- javascript.js
+++ javascript.js
@@ -3,22 +3,24 @@ const switchButton = document.getElementById('switch-button');
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
 
 function applyDarkMode() {
-    document.body.classList.add('dark-mode');
+  document.body.classList.add('dark-mode');
 }
+
 function removeDarkMode() {
-    document.body.classList.remove('dark-mode');
+  document.body.classList.remove('dark-mode');
 }
+
 function switchMode() {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    if (isDarkMode) {
-        removeDarkMode();
-        localStorage.setItem('darkMode', false);
-    } else {
-        applyDarkMode();
-        localStorage.setItem('darkMode', true);
-    }
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  if (isDarkMode) {
+    removeDarkMode();
+    localStorage.setItem('darkMode', false);
+  } else {
+    applyDarkMode();
+    localStorage.setItem('darkMode', true);
+  }
 }
 switchButton.addEventListener('click', switchMode);
 if (isDarkMode) {
-    applyDarkMode();
+  applyDarkMode();
 }
